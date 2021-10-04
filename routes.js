530b44033263b01fbb5ca4480beda7acfeaa6c39
TRBhavaniShankar <tr.bhavanishankar93@gmail.com>
2021-10-04 14:10:17 +0530

    WIP: mongoose db

--- routes.js
+++ routes.js
@@ -1,6 +1,45 @@
 const express = require("express");
 const {PostModel, AvatarModel} = require("./models");
+var cors = require('cors')
 const app = express();
+
+const data = [
+    {
+        "_id": "61594c60b362a72fcc3d39ee",
+        "body": "this is test 4",
+        "avatar_id": "4",
+        "created_at": "2021-10-03T06:23:28.676Z",
+        "__v": 0
+    },
+    {
+        "_id": "61594a664a5c82030ccdac76",
+        "body": "this is test 1",
+        "avatar_id": "3",
+        "created_at": "2021-10-03T06:15:02.015Z",
+        "__v": 0
+    },
+    {
+        "_id": "6159478e22144acffa188e9a",
+        "body": "this is test",
+        "avatar_id": "2",
+        "created_at": "2021-10-03T06:02:55.003Z",
+        "__v": 0
+    },
+    {
+        "_id": "6159463b3dbffdab684f13ab",
+        "body": "test",
+        "avatar_id": "test/1",
+        "created_at": "2021-10-03T05:57:15.442Z",
+        "__v": 0
+    },
+    {
+        "_id": "61591bc7c29cf456abd95d89",
+        "body": "hellow world!!",
+        "created_at": "2021-10-02T18:30:00.000Z"
+    }
+]
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("Hello world")
@@ -25,6 +64,7 @@ app.get('/posts', async (req, res) => {
         }
         const post = await PostModel.find().sort({created_at: sort});
         res.send(post);
+        // res.send(data)
     } catch (error) {
         res.status(500).send(error);
     }
